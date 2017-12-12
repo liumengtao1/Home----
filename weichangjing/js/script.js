652fb9ef3f368421e2b6aaf1e593f67d8578c03b
liumengtao1 <2537750753@qq.com>
2017-12-12 17:27:47 +0800

    第四屏

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -26,4 +26,18 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
+var mp3 = document.querySelector("#mp3");
+var mp3btn = document.querySelector(".mp3btn");
+
+mp3btn.addEventListener('click', function () {
+    if (mp3.paused) {
+        mp3.play();//播放
+        mp3btn.style.animationPlayState = 'running'; //图片旋转
+    } else {
+        mp3.pause();//暂停
+        mp3btn.style.animationPlayState = 'paused';  // 暂停
+    }
+})
+
+
 
