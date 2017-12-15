4cbc0ac79fb4173ba58ada3d1b3b062ea6eed3e8
liumengtao1 <2537750753@qq.com>
2017-12-15 15:12:58 +0800

    修改邀请函

--- yaoqinghan/js/script.js
+++ yaoqinghan/js/script.js
@@ -1,16 +1,21 @@
 var swiper = new Swiper('.swiper-container', {
     //Slides的滑动方向，可设置水平(horizontal)或垂直(vertical)。
     direction: 'vertical',
-    //分页器
+    /*//分页器
     pagination: {
         el: '.swiper-pagination',
         //让分页器支持点击
         clickable: true,
-    },
+    },*/
 
     // 前进按钮
     navigation: {
         nextEl: '.swiper-button-next',
+    },
+
+    // 如果需要滚动条
+    scrollbar: {
+        el: '.swiper-scrollbar',
     },
 
     //添加动画效果
